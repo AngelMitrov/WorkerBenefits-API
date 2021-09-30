60d538b9b04267db1416a8d716648a1e416a036a
AngelMitrov <angelmitrov2001@yahoo.com>
2021-09-30 17:55:28 +0200

    Added Username and Pass

---
+++ WebApi.WorkerBenefits.DataAccess/Migrations/20210930155212_AddedUsernameAndPasswordToWorker.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.WorkerBenefits.DataAccess.Migrations
+{
+    public partial class AddedUsernameAndPasswordToWorker : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Workers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Workers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Workers");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Workers");
+        }
+    }
+}
--- WebApi.WorkerBenefits.Domain/Models/Worker.cs
+++ WebApi.WorkerBenefits.Domain/Models/Worker.cs
@@ -4,6 +4,8 @@
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public int Age { get; set; }
         public int JobPositionId { get; set; }
         public JobPosition JobPosition { get; set; }
